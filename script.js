3ffef864a80f8f4ec18c58614c0048c5f062983e
dorineachieng682-ship-it <dorineachieng682@gmail.com>
2026-03-23 09:25:01 +0300

    second commit

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ document.querySelectorAll('a').forEach(anchor => {
     }
   });
 });
+
+// EMAILJS CONFIG
+(function(){
+  emailjs.init("YOUR_PUBLIC_KEY"); // replace this
+})();
+
+document.getElementById("contact-form")
+.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  emailjs.sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", this)
+    .then(function() {
+      alert("Message sent successfully!");
+    }, function(error) {
+      alert("Failed to send message.");
+    });
+});
